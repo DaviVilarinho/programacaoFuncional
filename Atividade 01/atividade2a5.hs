9dd1b48ed11ac76250d0573765ab95359f22ef7c
vilarinux <davi.r.vilarinho@gmail.com>
2021-03-07 20:58:44 -0300

    tarefa e print concluídos

--- Atividade 01/atividade2a5.hs
+++ Atividade 01/atividade2a5.hs
@@ -10,9 +10,4 @@ hipotenusa b c = sqrt(b^2 + c^2)
 -- questão 5
 distance (x1, y1) (x2, y2) = sqrt((x1-x2)^2 + (y1-y2)^2)
 
--- testes com a main das questões acima
-main = do
-    putStrLn $ "Dobro de 5 é: " dobra 5
-    putStrLn $ "Quádruplo de 6 é: " ++ quadruplica 6
-    putStrLn $ "A hipotenusa do triângulo retângulo formado pelos catetos 6 e 8 é: " ++ hipotenusa 6 8
-    putStrLn $ "A distância entre os pontos (1,1) e (2,2) é: " ++ distance (1,1) (2,2)
+-- testes por meio do ghci
